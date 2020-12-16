4a9a13224d9a575e0699bd5281e17291dce43d99
Boyan.Ivanov <boyan.ivanov@perspectum.com>
2020-12-16 10:06:57 +0000

    Removed non-linear fitting and implemented data plotting // Need to account for diffusion direction

--- main.py
+++ main.py
@@ -20,160 +20,6 @@ from tqdm import tqdm
 import pandas as pd  # To read data
 from sklearn.linear_model import LinearRegression
 
-def Model(x, S0,bv): 
-        return  S0*np.exp(bv * (-x))
-def Fit(px,bv):
-        t1_start = process_time() 
-                
-        ItNum = 800 # Maximum Number of iterations for each point
-        
-        bnds = ([0], [np.inf])
-        
-        #Data =  loadmat('var')
-        
-        yDLin = px
-        xDLin = bv
-        
-        yD = px
-        sz = yD.shape
-        yD = np.reshape(yD,[sz[0]*sz[1], sz[2]],order='F')
-        yD = yD.tolist()
-        yD = np.asarray(yD)
-        yD = yD.astype(np.float64)
-        yD = yD.T
-
-        
-        xD = bv
-        xD = xD.tolist()
-        xD = np.asarray(xD)
-        xD = np.reshape(xD,[len(xD.T)],order='F')
-        xD = xD.astype(np.float64)
-        xD = xD.T
-        
-        S0 = []
-        adc = []
-        c = 0
-        
-        # Functions section 
-        def func(x, bv, S0): 
-                return  S0*np.exp(bv * (-x))
-        
-        def grad(t,a,b):
-                d1 = np.exp(a * (-t))
-                d2 = -np.exp(a*(-t))*t*b
-                return np.array([d2,d1]).T
-
-        # function for genetic algorithm to minimize (sum of squared error)
-        def sumOfSquaredError(parameterTuple):
-                warnings.filterwarnings("ignore") # do not print warnings by genetic algorithm
-                val = func(xD, *parameterTuple)   
-                return np.sum((row - val) ** 2.0)
-        
-        # generate initial parameter values
-        P1,P2 = LinFit(yDLin,xDLin)
-        Error = []
-        Rows = []
-        ##########################################################################################
-        # Iterating through the matrix row by row
-        for row in yD.T: 
-                if c == yD.shape[1]-1:
-                        
-                                # curve fit the test data
-                                p1 = P1.T[c]
-                                p2 = P2[c]
-
-                                RowFit,pocv = curve_fit(func, xD,row, p0=[p1,p2],maxfev=ItNum,
-                                                        method='lm',absolute_sigma='True'
-                                                        ,jac=grad)              
-                                S0.append(RowFit[1])
-                                adc.append(RowFit[0]) 
-                                Rows.append(RowFit)  
-                                modelPredictions = func(xD, *RowFit) 
-
-                                absError = modelPredictions - row
-                               
-                                SE = np.square(absError) # squared errors
-                                MSE = np.mean(SE) # mean squared errors
-                                RMSE = np.sqrt(MSE) # Root Mean Squared Error, RMSE
-                                Rsquared = 1.0 - (np.var(absError) / np.var(row))
-                                Error.append(Rsquared)
-                                #print('\n Row ',c,'out of ',yD.shape[1])
-                                #print('\n Parameters: ', RowFit)
-                                print('\n Rsquared',Rsquared)
-                                #print('\n Sleep')  
-                                                    
-                                c += 1
-                                break
-                else:                        
-                                # curve fit the test data
-                                p1 = P1.T[c]
-                                p2 = P2[c]
-
-                                RowFit,pocv = curve_fit(func,xD,row, p0=[p1,p1],maxfev=ItNum,
-                                                        method='lm',absolute_sigma='True',jac=grad
-                                                    )              
-                                S0.append(RowFit[1])
-                                adc.append(RowFit[0]) 
-                                Rows.append(RowFit)  
-                                modelPredictions = func(xD, *RowFit) 
-
-                                absError = modelPredictions - row
-                               
-                                SE = np.square(absError) # squared errors
-                                MSE = np.mean(SE) # mean squared errors
-                                RMSE = np.sqrt(MSE) # Root Mean Squared Error, RMSE
-                                Rsquared = 1.0 - (np.var(absError) / np.var(row))
-                                Error.append(Rsquared)
-                                #print('\n Row ',c,'out of ',yD.shape[1])
-                                #print('\n Parameters: ', RowFit)
-                                print('\n Rsquared',Rsquared)    
-                                                  
-                                c += 1
-                
-                
-        t1_stop = process_time()         
-        Error = np.asarray(Error)
-        Sum = np.sum(Error)
-        Mean = Sum/yD.shape[1]
-        print('\n The mean Rsquared is: ', Mean)
-        print("Elapsed time during the whole program in seconds:", 
-                                         t1_stop-t1_start)
-        S0 = np.asarray(S0)
-        S0 = np.reshape(S0,[yDLin.shape[0], yDLin.shape[1]], order='F')
-        adc = np.asarray(adc)
-        adc = np.reshape(adc,[yDLin.shape[0], yDLin.shape[1]], order='F')
-        ##########################################################################################
-        
-        # Graphics output section
-        
-        def ModelAndScatterPlot(graphWidth, graphHeight):
-                f = plt.figure(figsize=(graphWidth/100.0, graphHeight/100.0), dpi=100)
-                axes = f.add_subplot(111)
-        
-        # first the raw data as a scatter plot
-                axes.plot(xD, yD,  'r+')
-
-        # create data for the fitted equation plot
-                xModel = np.linspace(min(xD), max(xD))
-                yModel = func(xModel, *RowFit)
-
-        # now the model as a line plot
-                axes.plot(xModel, yModel)
-
-                axes.set_xlabel('bvalues') # X axis data label
-                axes.set_ylabel('Y Data') # Y axis data label
-
-                plt.show()
-                plt.close('all') # clean up after using pyplot
-
-        graphWidth = 800
-        graphHeight = 600
-        ModelAndScatterPlot(graphWidth, graphHeight)
-        
-        xModel = np.linspace(min(xD), max(xD))
-        yModel = func(xModel, *RowFit)
-        
-        return S0,adc
 def LinFit(DiffImage, bv):
                 sz = DiffImage.shape  
                 
@@ -197,12 +43,7 @@ def LinFit(DiffImage, bv):
                 where_are_NaNs = np.isnan(logS0_ADC)
                 logS0_ADC[where_are_NaNs] = 0
                 
-                lr = LinearRegression()
-                lr.fit(bv,logS)
-                
-                Y_Pred = lr.predict(bv)
-
-                return logS0_ADC,S0
+                return logS0_ADC,S0,S
 
 def Scratch():
         if platform.system() == "Windows":
@@ -261,10 +102,22 @@ def Scratch():
         for image in tqdm(SortedImages):
                 ImageMatrix = np.transpose(np.asarray(SortedImages[image]))
                 bv = np.asarray(SortedBvals[image])
-                logS0_ADC,S0 = LinFit(ImageMatrix,bv)
+                logS0_ADC,S0,S= LinFit(ImageMatrix,bv)
                 logS0_ADC = np.reshape(logS0_ADC,[172,172])
                 Fitted_Images.append(logS0_ADC)
         Fitted_Images = np.asarray(Fitted_Images)
+        
+        lr = LinearRegression()
+        lr.fit(bv.reshape(-1,1),S.T)        
+        Y_Pred = lr.predict(bv.reshape(-1,1))
+        
+        ImagePixel = ImageMatrix[80,80,:]
+        Y_Pred = np.reshape(Y_Pred,[30,172,172])
+        plt.scatter(bv,ImagePixel)
+        plt.plot(bv,Y_Pred[:,80,80],color='red')
+        plt.show()
+        
+        
         
        # Code to create .gif file of the fitted images
         print("Creting GIF image...")
