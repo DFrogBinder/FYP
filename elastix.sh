943f358b4c7233ffffb574cae46aca620027057d
Boyan.Ivanov <boyan.ivanov@perspectum.com>
2021-01-24 21:01:22 +0000

    Updating the .sh file

--- elastix.sh
+++ elastix.sh
@@ -1,4 +1,4 @@
 export DYLD_LIBRARY_PATH='/Users/boyanivanov/Elastix/lib':$DYLD_LIBRARY_PATH
-output=$(eval "elastix") 
+output=$(eval "elastix -m /Users/boyanivanov/Desktop/FYP/DICOM/Leeds_Patient_4128010/19/DICOM/T1_slice_3_MoCoMo/Original/001.mhd -f /Users/boyanivanov/Desktop/FYP/DICOM/Leeds_Patient_4128010/19/DICOM/T1_slice_3_MoCoMo/Fitted/001.mhd -out /Users/boyanivanov/Desktop/FYP/DICOM/Leeds_Patient_4128010/19/DICOM/T1_slice_3_MoCoMo/BSplines_Registered_1 -p /Users/boyanivanov/Desktop/FYP/DICOM/Elastix_Parameters_Files/BSplines_T1.txt") 
 echo "$output" 
 
