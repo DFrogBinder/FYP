73f9eb4dbb8652987b7acbb938f82c7c2e34e776
Boyan.Ivanov <boyan.ivanov@perspectum.com>
2021-08-08 08:29:36 +0100

    Added plotting code // Still has a bug with slice 5

--- Extract_Loss.py
+++ Extract_Loss.py
@@ -41,20 +41,22 @@ def Extract_Loss(path):
                 if i == 'loss:':
                     Slice_Data['4'].append(float(StrSplit[k+1]))
     
-    Slice_Data['4'] = np.unique(Slice_Data['4']) # Filters repeating data
+    #Slice_Data['4'] = np.unique(Slice_Data['4']) # Filters repeating data
     for i in range(5):
         Slice_Data[str(i)] = np.asarray(Slice_Data[str(i)])
     
+    '''
     kernel_size = 100
     kernel = np.ones(kernel_size) / kernel_size
     data_convolved = np.convolve(Extracted_Data, kernel, mode='same')   
-    X_data = range(len(Extracted_Data))      
+    '''
+    X_data = range(len(Slice_Data['0']))      
+    X_data_sp = range(len(Slice_Data['4']))
+    fig,ax=plt.subplots()       
+    ax.plot(X_data,Slice_Data['0'])
     
     fig,ax=plt.subplots()       
-    ax.plot(X_data,Extracted_Data)
-    
-    fig,ax=plt.subplots()       
-    ax.plot(X_data,data_convolved)
+    ax.plot(X_data_sp,Slice_Data['4'])
     plt.show()
 
 Extract_Loss('/Users/boyanivanov/log/Log.txt')
