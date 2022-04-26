05933c9a38d63ecb3586b077969f8e6c0eacd4fb
DFrogBinder <boyan.ivanov32@abv.bg>
2022-04-26 12:26:41 +0100

    remove cli functionality for dice coef function

--- Re-Slicing/Dice_Coef.py
+++ Re-Slicing/Dice_Coef.py
@@ -40,17 +40,7 @@ def get_hausdorff(Im1,Im2):
   distance = scipy.spatial.distance.directed_hausdorff(Im1,Im2)
   return distance
 
-def main(Path):
-  # Check is we have a full segmentation 
-    Data = Load_data(Path)
-    if Data[:,:,1].any()==False:
-      print('The file provided does not contain a full segmentation!')
-    else:
-      Data = get_extended_dice(Data)
-
-    #Im1 = Data[:,:,56] #56 ; 32
-    #Im2 = Data[:,:,57] #57 ; 33
-
+def main(Im1,Im2):
 
     Dice_coef = get_dice_coefficient(Im1,Im2)
     Hausdorff = get_hausdorff(Im1,Im2)
@@ -58,8 +48,9 @@ def main(Path):
     print("Hausdorff distance is: " + str(Hausdorff[0]))
     return Dice_coef, Hausdorff
 
+'''
 parser = argparse.ArgumentParser(description='Process data path.')
-parser.add_argument('-p',type=str,help='The arguments file in nifti format')
+parser.add_argument('-f',type=str,help='The arguments file in nifti format')
 #parser.add_argument('-f',action='store_true',help="""Set flag only if the full
 #                                            set of images are segmented.""")
 
@@ -68,3 +59,4 @@ if __name__ == '__main__':
     # Uncoment to provide hard-coded path and for debugging 
     # args.p = '/Volumes/T7/Internal_Report_Data/Internal_Report_Graphics/P29_Original_Segmentation/P29_Full-Segmentation.nii.gz'
     main(args.p)
+'''
