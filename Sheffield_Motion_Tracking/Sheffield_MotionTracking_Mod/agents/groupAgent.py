3d5adb6c045846a4002a89dfa8d398f65c869f46
DFrogBinder <boyan.ivanov32@abv.bg>
2022-04-26 12:23:10 +0100

    Muting the batch size print made in the prevoious commits

--- Sheffield_Motion_Tracking/Sheffield_MotionTracking_Mod/agents/groupAgent.py
+++ Sheffield_Motion_Tracking/Sheffield_MotionTracking_Mod/agents/groupAgent.py
@@ -169,7 +169,7 @@ class groupAgent(BaseAgent):
             # grid = torchvision.utils.make_grid(batch_mri, nrow=5)
             # plt.imshow(grid.cpu().permute(1, 2, 0)); plt.axis('off')
             # plt.imsave("Image.png", np.asarray(grid.cpu().permute(1, 2, 0)), format='png')
-            print(batch_resampled.shape)
+            # print(batch_resampled.shape)
 
             # Forward pass
             res = self.model(batch_mri)
