57aefe5d11ba0cbd588909f2f22faf988ae8e934
DFrogBinder <boyan.ivanov32@abv.bg>
2022-05-04 10:46:11 +0100

    fixing save path

--- Sheffield_Motion_Tracking/Sheffield_MotionTracking_Mod/agents/groupAgent.py
+++ Sheffield_Motion_Tracking/Sheffield_MotionTracking_Mod/agents/groupAgent.py
@@ -240,7 +240,7 @@ class groupAgent(BaseAgent):
         copy_warped_input_image = sitk.GetImageFromArray(copy_warped_input_image.cpu())
         copy_warped_input_image.SetSpacing(sitk.ReadImage(train_batch["image"][tio.PATH][0]).GetSpacing())
         copy_warped_input_image.SetDirection(sitk.ReadImage(train_batch["image"][tio.PATH][0]).GetDirection())
-        sitk.WriteImage(copy_warped_input_image, os.path.join(patient_output_path, FixedImageName))
+        sitk.WriteImage(copy_warped_input_image, os.path.join(FixedImageName))
 
         print('Fixed Image exported!')
 
