4f2b7ba9e1404786efaa8609afb3fa457ee77503
DFrogBinder <boyan.ivanov32@abv.bg>
2022-05-25 23:27:30 +0100

    Listing all available images

--- Sheffield_Motion_Tracking/Sheffield_MotionTracking_Mod/agents/groupAgent.py
+++ Sheffield_Motion_Tracking/Sheffield_MotionTracking_Mod/agents/groupAgent.py
@@ -235,6 +235,7 @@ class groupAgent(BaseAgent):
         copy_warped_input_image = res['template'].clone().detach()
         copy_warped_input_image = copy_warped_input_image[:,0,:,:]
         print("Warped Input Image Shape is: "+str(copy_warped_input_image.shape))
+        print("List of all available images: "+res)
 
         FixedImageName = str('wimage'+str(self.current_epoch)+'.mha')
 
