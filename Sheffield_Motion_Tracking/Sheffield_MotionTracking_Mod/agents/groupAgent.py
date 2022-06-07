ceb98d3606c45a014ffd037491210c1d832aa4bd
DFrogBinder <boyan.ivanov32@abv.bg>
2022-06-08 00:09:13 +0100

    some more logging

--- Sheffield_Motion_Tracking/Sheffield_MotionTracking_Mod/agents/groupAgent.py
+++ Sheffield_Motion_Tracking/Sheffield_MotionTracking_Mod/agents/groupAgent.py
@@ -155,6 +155,7 @@ class groupAgent(BaseAgent):
 
         for train_batch in self.train_loader:
             print("Slef.train_loader is: "+str(type(self.train_loader)))
+            print('Type of Train_Batch is '+str(train_batch))
             print("Train_Batch is: "+str(len(train_batch['image'])))
             # switch model to training mode, clear gradient accumulators
             self.model.train()
